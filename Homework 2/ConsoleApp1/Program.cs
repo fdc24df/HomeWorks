68dd1df26a921207cfd0c1ee919fe14da13adb02
Custo <sabaglonti1995@gmail.com>
2025-09-16 02:41:29 +0400

    pushing

---
+++ Homework 2/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+
+
+        Console.WriteLine("author: Saba Glonti");
+
+
+        Console.Write("Enter some text: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("your text: " + input);
+        Console.Beep();
+    }
+}
